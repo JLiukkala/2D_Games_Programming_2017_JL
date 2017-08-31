7ed240c51d8779acb7862f215fb555664d88a63d
Jaakko Liukkala <jaakko.liukkala@cs.tamk.fi>
2017-08-31 13:47:45 +0300

    Spaceship plugin and player spaceship input and movement.

---
+++ Space Shooter/Assets/Code/PlayerSpaceship.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class PlayerSpaceship : MonoBehaviour
+    {
+
+        public float Speed = 1.5f; 
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 movementVector = GetMovementVector();
+            transform.Translate(movementVector * Speed * Time.deltaTime);
+        }
+
+        private Vector3 GetMovementVector()
+        {
+            Vector3 movementVector = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                movementVector += Vector3.left;
+            }
+
+            //if(Input.GetAxis("Horizontal"))
+            //{
+            //    movementVector += Vector3.right;
+            //}
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                movementVector += Vector3.right;
+            }
+
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                movementVector += Vector3.up;
+            }
+
+            if(Input.GetKey(KeyCode.DownArrow))
+            {
+                movementVector += Vector3.down;
+            }
+
+            return movementVector;
+        }
+    }
+}

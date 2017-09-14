0e3c5cc1d258ca98be652377c8f2ad9cc6824d9a
Jaakko Liukkala <jaakko.liukkala@cs.tamk.fi>
2017-09-14 13:47:03 +0300

    The game after the third lecture.

---
+++ Space Shooter/Assets/Code/Projectile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class Projectile : MonoBehaviour, IDamageProvider
+    {
+        [SerializeField]
+        private int _damage;
+        [SerializeField]
+        private float _speed;
+
+        private Rigidbody2D _rigidBody;
+        private Vector2 _direction;
+        private bool _isLaunched = false;
+
+        protected virtual void Awake()
+        {
+            _rigidBody = GetComponent<Rigidbody2D>();
+
+            if(_rigidBody == null)
+            {
+                Debug.LogError("No RigidBody2D component was found from the GameObject!");
+            }
+        }
+
+        protected void FixedUpdate()
+        {
+            if (!_isLaunched)
+            {
+                return;
+            }
+
+            Vector2 velocity = _direction * _speed;
+            Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+            Vector2 newPosition = currentPosition + velocity * Time.fixedDeltaTime;
+            _rigidBody.MovePosition(newPosition);
+        }
+
+        public void Launch(Vector2 direction)
+        {
+            _direction = direction;
+            _isLaunched = true;
+        }
+
+        public int GetDamage()
+        {
+            return _damage;
+        }
+    }
+}
+
---
+++ Space Shooter/Assets/Code/Weapon.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField]
+        private float _cooldownTime = 0.5f;
+        [SerializeField]
+        private Projectile _projectilePrefab;
+
+        private float _timeSinceShot = 0;
+        private bool _isInCooldown = false;
+
+        public bool Shoot()
+        {
+            if (_isInCooldown)
+            {
+                return false;
+            }
+
+            Projectile projectile = Instantiate(_projectilePrefab, transform.position, transform.rotation);
+            projectile.Launch(transform.up);
+
+            _isInCooldown = true;
+            _timeSinceShot = 0;
+
+            return true;
+        }
+
+        void Update()
+        {
+            if(_isInCooldown)
+            {
+                _timeSinceShot += Time.deltaTime;
+
+                if(_timeSinceShot >= _cooldownTime)
+                {
+                    _isInCooldown = false;
+                }
+            }
+        }
+    }
+}
